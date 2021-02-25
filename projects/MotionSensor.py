12a3fe861788847238822b6e958ad4ae58eff759
n01291955 <n01291955@humbermail.ca>
2021-02-25 17:40:53 -0500

    Added combined code for sensors

---
+++ projects/MotionSensor.py
@@ -0,0 +1,41 @@
+# MBTechWorks.com 2017
+# Use an HC-SR501 PIR to detect motion (infrared)
+
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD) #Set GPIO to pin numbering
+pir = 8 #Assign pin 8 to PIR
+led = 10 #Assign pin 10 to LED
+GPIO.setup(pir, GPIO.IN) #Setup GPIO pin PIR as input
+GPIO.setup(led, GPIO.OUT) #Setup GPIO pin for LED as output
+print ("Sensor initializing . . .")
+time.sleep(3) #Give sensor time to startup
+print ("Active")
+print ("Press Ctrl+c to end program")
+n=1
+try:
+    while True:
+        if GPIO.input(pir) == True: #If PIR pin goes high, motion is detected
+            print ("Motion Detected!")
+            GPIO.output(led, True) #turn on LED
+            time.sleep(6) #Keep LED on for 4 seconds
+            GPIO.output(led, False) #Turn off led
+            n+=1
+            print("Counter: "+str(n))
+        else:
+            print("No motion was detected")
+            time.sleep(6)
+
+except KeyboardInterrupt: #Ctrl+c
+    pass #Do nothing, continue to finally
+
+finally:
+    GPIO.output(led, False) #Turn off LED in case left on
+    GPIO.cleanup() #reset all GPIO
+    print ("Program ended")
+    
+    
+    
---
+++ projects/README.md
@@ -0,0 +1,7 @@
+# SmartHome
+Smart Home Project
+## Group members
+* Alen Alimkhanov
+* Heorhii Nechyporenko
+* Mykyta Nechyporenko
+* Nikita Smirnov
