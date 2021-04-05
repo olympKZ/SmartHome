f2074ad559990abdce8684c9c1d18ffb3faebd8f
n01291955 <71232215+n01291955@users.noreply.github.com>
2021-04-04 22:18:35 -0400

    Final Version of Android App

---
+++ SmartHomeApp/SHApp/app/src/main/java/com/example/smarthomeapp/User.java
@@ -0,0 +1,14 @@
+package com.example.smarthomeapp;
+
+public class User {
+
+    public String email;
+    public User(){
+
+    }
+
+    public User(String email){
+        this.email = email;
+    }
+
+}
